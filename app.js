839d527f2b0d4b000be90d255be09d927d23f027
ruyang <russelyang@gmail.com>
2018-08-27 18:41:50 -0700

    enable graphigl

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const schema = require('./schema/schema');
 const app = express();
 
 app.use('/graphql', graphHttpServer({
-    schema
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000, () => {
